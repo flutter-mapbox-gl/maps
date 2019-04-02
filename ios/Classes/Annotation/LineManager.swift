76afa49bef14cc600015c8135200e453407bc979
Timothy Sealy <timothy.sealy@gmail.com>
2019-04-03 00:20:14 +0200

    Implemented data driven line properties.

--- ios/Classes/Annotation/LineManager.swift
+++ ios/Classes/Annotation/LineManager.swift
@@ -9,5 +9,20 @@ class LineManager: AnnotationManager<LineGeometry> {
         super.init(sourceId: ID_GEOJSON_SOURCE)
         
         layer = MGLLineStyleLayer(identifier: ID_GEOJSON_LAYER, source: source)
+        setDataDrivenLayerProperties()
+    }
+    
+    func setDataDrivenLayerProperties() {
+        if let layer = layer {
+            layer.lineJoin = NSExpression(forKeyPath: LineOptions.KEY_LINE_JOIN)
+            layer.lineOpacity = NSExpression(forKeyPath: LineOptions.KEY_LINE_OPACITY)
+            layer.lineColor = NSExpression(forKeyPath: LineOptions.KEY_LINE_COLOR)
+            layer.lineWidth = NSExpression(forKeyPath: LineOptions.KEY_LINE_WIDTH)
+            layer.lineGapWidth = NSExpression(forKeyPath: LineOptions.KEY_LINE_GAP_WIDTH)
+            layer.lineOffset = NSExpression(forKeyPath: LineOptions.KEY_LINE_OFFSET)
+            layer.lineBlur = NSExpression(forKeyPath: LineOptions.KEY_LINE_BLUR)
+            //FIX: Setting the line pattern does not show a line.
+//            layer.linePattern = NSExpression(forKeyPath: LineOptions.KEY_LINE_PATTERN)
+        }
     }
 }
