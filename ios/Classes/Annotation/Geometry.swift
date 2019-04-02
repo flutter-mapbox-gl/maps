dd70bf6a4faf05e0ab0b7c100f7ea0bbd7090e05
Timothy Sealy <timothy.sealy@gmail.com>
2019-04-02 20:43:11 +0200

    Added GeoJSON datatypes (swift protocols). The Geometry inherits Encodable for easy JSON parsing.

---
+++ ios/Classes/Annotation/Geometry.swift
@@ -0,0 +1,29 @@
+// Geometry protocols
+
+protocol Geometry: Encodable {
+    var type: String { get }
+}
+
+protocol Point: Geometry {
+    var coordinates: [Double] { get set }
+}
+
+protocol MultiPoint: Geometry {
+    var coordinates: [[Double]] { get set }
+}
+
+protocol LineString: Geometry {
+    var coordinates: [[Double]] { get set }
+}
+
+protocol MultiLineString: Geometry {
+    var coordinates: [[[Double]]] { get set }
+}
+
+protocol Polygon: Geometry {
+    var coordinates: [[[Double]]] { get set }
+}
+
+protocol MultiPolygon: Geometry {
+    var coordinates: [[[Double]]] { get set }
+}
