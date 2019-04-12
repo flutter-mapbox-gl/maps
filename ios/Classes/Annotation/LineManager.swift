bacb0da539a897550ae0e7ae83957ececcfb5489
Timothy Sealy <timothy.sealy@gmail.com>
2019-04-12 22:20:53 +0200

    Setting layer data driven properties based supplied options.

--- ios/Classes/Annotation/LineManager.swift
+++ ios/Classes/Annotation/LineManager.swift
@@ -7,22 +7,45 @@ class LineManager: AnnotationManager<LineGeometry> {
     
     init() {
         super.init(sourceId: ID_GEOJSON_SOURCE)
-        
         layer = MGLLineStyleLayer(identifier: ID_GEOJSON_LAYER, source: source)
-        setDataDrivenLayerProperties()
     }
     
-    func setDataDrivenLayerProperties() {
-        if let layer = layer {
+    func create(options: LineOptions) -> Line? {
+        setDataDrivenLayerProperties(options: options)
+        return super.create(options: options)
+    }
+    
+    func update(id: UInt64, options: LineOptions) {
+        setDataDrivenLayerProperties(options: options)
+        super.update(id:id, options: options)
+    }
+    
+    func setDataDrivenLayerProperties(options: LineOptions) {
+        guard let layer = layer else { return }
+        
+        if let _ = options.lineJoin {
             layer.lineJoin = NSExpression(forKeyPath: LineOptions.KEY_LINE_JOIN)
+        }
+        if let _ = options.lineOpacity {
             layer.lineOpacity = NSExpression(forKeyPath: LineOptions.KEY_LINE_OPACITY)
+        }
+        if let _ = options.lineColor {
             layer.lineColor = NSExpression(forKeyPath: LineOptions.KEY_LINE_COLOR)
+        }
+        if let _ = options.lineWidth {
             layer.lineWidth = NSExpression(forKeyPath: LineOptions.KEY_LINE_WIDTH)
+        }
+        if let _ = options.lineGapWidth {
             layer.lineGapWidth = NSExpression(forKeyPath: LineOptions.KEY_LINE_GAP_WIDTH)
+        }
+        if let _ = options.lineOffset {
             layer.lineOffset = NSExpression(forKeyPath: LineOptions.KEY_LINE_OFFSET)
+        }
+        if let _ = options.lineBlur {
             layer.lineBlur = NSExpression(forKeyPath: LineOptions.KEY_LINE_BLUR)
-            //FIX: Setting the line pattern does not show a line.
-//            layer.linePattern = NSExpression(forKeyPath: LineOptions.KEY_LINE_PATTERN)
+        }
+        if let _ = options.linePattern {
+            layer.linePattern = NSExpression(forKeyPath: LineOptions.KEY_LINE_PATTERN)
         }
     }
 }
