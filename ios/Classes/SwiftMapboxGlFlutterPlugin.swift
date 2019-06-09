26e3fec19e165cd3dcd6383555e5e99cbe2c08f5
James Avyaz <james.ayvaz@gmail.com>
2019-06-09 01:02:31 -0700

    Added support for sideloading map tiles (#73)
    * Added support for sideloading map tiles

--- ios/Classes/SwiftMapboxGlFlutterPlugin.swift
+++ ios/Classes/SwiftMapboxGlFlutterPlugin.swift
@@ -5,5 +5,51 @@ public class SwiftMapboxGlFlutterPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = MapboxMapFactory(withMessenger: registrar.messenger())
         registrar.register(instance, withId: "plugins.flutter.io/mapbox_gl")
+
+        let channel = FlutterMethodChannel(name: "plugins.flutter.io/mapbox_gl", binaryMessenger: registrar.messenger())
+
+        channel.setMethodCallHandler { (methodCall, result) in
+            switch(methodCall.method) {
+            case "installOfflineMapTiles":
+                guard let arguments = methodCall.arguments as? [String: String] else { return }
+                let tilesdb = arguments["tilesdb"]
+                let assetkey = registrar.lookupKey(forAsset: tilesdb!)
+                installOfflineMapTiles(key: assetkey)
+                result(nil)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
+    }
+
+    private static func getTilesUrl() -> URL {
+        guard var cachesUrl = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first,
+            let bundleId = Bundle.main.object(forInfoDictionaryKey: kCFBundleIdentifierKey as String) as? String else {
+                fatalError("Could not get map tiles directory")
+        }
+        cachesUrl.appendPathComponent(bundleId)
+        cachesUrl.appendPathComponent(".mapbox")
+        cachesUrl.appendPathComponent("cache.db")
+        return cachesUrl
+    }
+
+    // Copies the "offline" tiles to where Mapbox expects them
+    private static func installOfflineMapTiles(key: String) {
+        var tilesUrl = getTilesUrl()
+        let bundlePath = Bundle.main.path(forResource: key, ofType: nil)
+        NSLog("Cached tiles not found, copying from bundle... \(String(describing: bundlePath)) ==> \(tilesUrl)")
+        do {
+            let parentDir = tilesUrl.deletingLastPathComponent()
+            try FileManager.default.createDirectory(at: parentDir, withIntermediateDirectories: true, attributes: nil)
+            if FileManager.default.fileExists(atPath: tilesUrl.path) {
+                try FileManager.default.removeItem(atPath: tilesUrl.path)
+            }
+            try FileManager.default.copyItem(atPath: bundlePath!, toPath: tilesUrl.path)
+            var resourceValues = URLResourceValues()
+            resourceValues.isExcludedFromBackup = true
+            try tilesUrl.setResourceValues(resourceValues)
+        } catch let error {
+            NSLog("Error copying bundled tiles: \(error)")
+        }
     }
 }
