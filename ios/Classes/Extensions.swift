c46e5f6c0a7c6cb131caaf82d972cbc2b1023da4
Timothy Sealy <timothy.sealy@gmail.com>
2019-10-24 23:47:03 +0200

    Fix for color parsing.

--- ios/Classes/Extensions.swift
+++ ios/Classes/Extensions.swift
@@ -47,15 +47,15 @@ extension UIColor {
             let start = hexString.index(hexString.startIndex, offsetBy: 1)
             let hexColor = hexString[start...]
             
-            if hexColor.count == 8 {
+            if hexColor.count == 6 {
                 let scanner = Scanner(string: String(hexColor))
                 var hexNumber: UInt64 = 0
                 
                 if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    b = CGFloat((hexNumber & 0x0000ff)) / 255
+                    a = 255
                     
                     self.init(red: r, green: g, blue: b, alpha: a)
                     return
