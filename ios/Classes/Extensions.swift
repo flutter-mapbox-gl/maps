45bab42c77acbce62f2559fd01eb89f3ac1dd16f
Timothy Sealy <timothy.sealy@gmail.com>
2019-10-24 23:00:56 +0200

    Added extension for parsing a UIColor from a hex string.

--- ios/Classes/Extensions.swift
+++ ios/Classes/Extensions.swift
@@ -38,3 +38,31 @@ extension MGLCoordinateBounds {
         return MGLCoordinateBounds(sw: southwest, ne: northeast)
     }
 }
+
+extension UIColor {
+    public convenience init?(hexString: String) {
+        let r, g, b, a: CGFloat
+        
+        if hexString.hasPrefix("#") {
+            let start = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hexColor = hexString[start...]
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: String(hexColor))
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
