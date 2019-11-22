08124f97317079cf47e510824adff8765c04c4e4
Timothy Sealy <timothy.sealy@gmail.com>
2019-11-22 14:00:12 +0100

    Added constants class for mapping string values of the symbol icon anchor to the enum equivalent.

---
+++ ios/Classes/Constants.swift
@@ -0,0 +1,21 @@
+import Mapbox
+import MapboxAnnotationExtension
+
+/*
+ * The mapping is based on the values defined here:
+ *  https://docs.mapbox.com/android/api/map-sdk/8.4.0/constant-values.html
+ */
+
+class Constants {
+    static let symbolIconAnchorMapping = [
+        "center": MGLIconAnchor.center,
+        "left": MGLIconAnchor.left,
+        "right": MGLIconAnchor.right,
+        "top": MGLIconAnchor.top,
+        "bottom": MGLIconAnchor.bottom,
+        "top-left": MGLIconAnchor.topLeft,
+        "top-right": MGLIconAnchor.topRight,
+        "bottom-left": MGLIconAnchor.bottomLeft,
+        "right": MGLIconAnchor.bottomRight
+    ]
+}
