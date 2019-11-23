10370fb0a7c95c3a9decabc26e1275b2f5c56c9d
Tobrun <tobrun.van.nuland@gmail.com>
2019-11-23 16:10:28 +0200

    Improve missing access token error message. (#117)
    * [android] - improve missing access token error message.
    
    * handle empty case

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
@@ -134,11 +134,15 @@ final class MapboxMapController
     try {
       ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
       Bundle bundle = ai.metaData;
-      return bundle.getString("com.mapbox.token");
-    } catch (PackageManager.NameNotFoundException e) {
-      Log.e(TAG, "Failed to load meta-data, NameNotFound: " + e.getMessage());
-    } catch (NullPointerException e) {
-      Log.e(TAG, "Failed to load meta-data, NullPointer: " + e.getMessage());
+      String token = bundle.getString("com.mapbox.token");
+      if (token == null || token.isEmpty()) {
+        throw new NullPointerException();
+      }
+      return token;
+    } catch (Exception e) {
+      Log.e(TAG, "Failed to find an Access Token in the Application meta-data. Maps may not load correctly. " +
+        "Please refer to the installation guide at https://github.com/tobrun/flutter-mapbox-gl#mapbox-access-token " +
+        "for troubleshooting advice." + e.getMessage());
     }
     return null;
   }
