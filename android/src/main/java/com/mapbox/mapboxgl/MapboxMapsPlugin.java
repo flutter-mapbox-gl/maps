2e4371400a3ce29c627048328350fec20c40336a
Cuberob <git@cuberob.com>
2020-01-20 15:36:05 +0100

    Fix MapboxMapsPlugin Nullpointer
    When the app is initialized in the background the
    activity provided by the Regisrar can be null,
    causing the app to crash. This commit checks if
    the activity is null before resolving it's hashCode.
    If it is null the initialization of
    this.registrarActivityHashCode is delayed until
    onActivityCreated is called.

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapsPlugin.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapsPlugin.java
@@ -27,7 +27,7 @@ public class MapboxMapsPlugin implements Application.ActivityLifecycleCallbacks 
   static final int STOPPED = 5;
   static final int DESTROYED = 6;
   private final AtomicInteger state = new AtomicInteger(0);
-  private final int registrarActivityHashCode;
+  private Integer registrarActivityHashCode = null;
 
   public static void registerWith(Registrar registrar) {
     final MapboxMapsPlugin plugin = new MapboxMapsPlugin(registrar);
@@ -44,6 +44,9 @@ public class MapboxMapsPlugin implements Application.ActivityLifecycleCallbacks 
 
   @Override
   public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    if(registrarActivityHashCode == null) {
+      registrarActivityHashCode = activity.hashCode();
+    }
     if (activity.hashCode() != registrarActivityHashCode) {
       return;
     }
@@ -95,6 +98,8 @@ public class MapboxMapsPlugin implements Application.ActivityLifecycleCallbacks 
   }
 
   private MapboxMapsPlugin(Registrar registrar) {
-    this.registrarActivityHashCode = registrar.activity().hashCode();
+    if(registrar.activity() != null) {
+      this.registrarActivityHashCode = registrar.activity().hashCode();
+    }
   }
 }
