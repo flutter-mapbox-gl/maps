eeb130af28f60030c3feccb08e8c1aa73b2a7594
Tobrun <tobrun.van.nuland@gmail.com>
2020-02-15 09:43:50 +0100

    [example] add full page map example (#201)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,8 +4,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:mapbox_gl_example/full_map.dart';
 
 import 'animate_camera.dart';
+import 'full_map.dart';
 import 'line.dart';
 import 'map_ui.dart';
 import 'move_camera.dart';
@@ -16,6 +18,7 @@ import 'scrolling_map.dart';
 
 final List<Page> _allPages = <Page>[
   MapUiPage(),
+  FullMapPage(),
   AnimateCameraPage(),
   MoveCameraPage(),
   PlaceSymbolPage(),
