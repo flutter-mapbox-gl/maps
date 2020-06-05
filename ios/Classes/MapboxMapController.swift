d6dc1232b7224bac4d6912d36bd4702dcff81b0c
Timothy Sealy <timothy.sealy@gmail.com>
2020-06-05 11:26:35 +0200

    Added symbol#getGeometry iOS implementation.

--- ios/Classes/MapboxMapController.swift
+++ ios/Classes/MapboxMapController.swift
@@ -195,6 +195,22 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
                 }
             }
             result(nil)
+        case "symbol#getGeometry":
+            guard let symbolAnnotationController = symbolAnnotationController else { return }
+            guard let arguments = methodCall.arguments as? [String: Any] else { return }
+            guard let symbolId = arguments["symbol"] as? String else { return }
+
+            var reply: [String:Double]? = nil
+            for symbol in symbolAnnotationController.styleAnnotations(){
+                if symbol.identifier == symbolId {
+                    if let geometry = symbol.geoJSONDictionary["geometry"] as? [String: Any],
+                        let coordinates = geometry["coordinates"] as? [Double] {
+                        reply = ["latitude": coordinates[1], "longitude": coordinates[0]]
+                    }
+                    break;
+                }
+            }
+            result(reply)
         case "circle#add":
             guard let circleAnnotationController = circleAnnotationController else { return }
             guard let arguments = methodCall.arguments as? [String: Any] else { return }
@@ -442,7 +458,6 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
         
        if let symbol = annotation as? Symbol {
             channel?.invokeMethod("symbol#onTap", arguments: ["symbol" : "\(symbol.id)"])
-        
         }
     }
     
