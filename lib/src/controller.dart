293a3c1641bb1107957c4d438783b79be04ef8ff
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2020-08-14 12:09:34 +0200

    Docs: document rounding behaviour

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -672,6 +672,10 @@ class MapboxMapController extends ChangeNotifier {
   }
 
   /// Returns the point on the screen that corresponds to a geographical coordinate ([latLng]). The screen location is in screen pixels (not display pixels) relative to the top left of the map (not of the whole screen)
+  /// 
+  /// Note: The resulting x and y coordinates are rounded to [int] on web, on other platforms they may differ very slightly (in the range of about 10^-10) from the actual nearest screen coordinate.
+  /// You therefore might want to round them appropriately, depending on your use case.
+  /// 
   /// Returns null if [latLng] is not currently visible on the map.
   Future<Point> toScreenLocation(LatLng latLng) async{
     return MapboxGlPlatform.getInstance(_id).toScreenLocation(latLng);
