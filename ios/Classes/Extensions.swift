b8deebc9ecee8e8a6390e6d3e0bfcbaa06007fae
Felix Horvat <felix.horvat@ocell.aero>
2021-03-03 20:28:29 +0100

    fixed brace issue

--- ios/Classes/Extensions.swift
+++ ios/Classes/Extensions.swift
@@ -114,7 +114,7 @@ extension UIColor {
                 if scanner.scanHexInt64(&hexNumber) {
                     r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
                     g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat(hexNumber & 0x0000ff00 >> 8) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
                     a = CGFloat(hexNumber & 0x000000ff) / 255
                     
                     self.init(red: r, green: g, blue: b, alpha: a)
