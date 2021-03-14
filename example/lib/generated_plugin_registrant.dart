43da90cd4d71afbe3cb09f285e695c27d718e3ae
Abhishek Shroff <ashroff6@gmail.com>
2021-03-14 15:24:51 +0530

    Update deprecated patterns to fix CI

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,15 +2,14 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'package:mapbox_gl_web/mapbox_gl_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  MapboxMapPlugin.registerWith(registry.registrarFor(MapboxMapPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  MapboxMapPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
