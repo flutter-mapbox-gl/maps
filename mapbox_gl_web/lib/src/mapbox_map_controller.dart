507b4df5b644c5ab997c5f0e232a518e00d1d030
Felix Horvat <felix.horvat@ocell.aero>
2021-11-23 22:29:36 +0100

    moved to non tiles mapbox api url

--- mapbox_gl_web/lib/src/mapbox_map_controller.dart
+++ mapbox_gl_web/lib/src/mapbox_map_controller.dart
@@ -1,7 +1,7 @@
 part of mapbox_gl_web;
 
 const _mapboxGlCssUrl =
-    'https://api.tiles.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css';
+    'https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css';
 
 class MapboxMapController extends MapboxGlPlatform
     implements MapboxMapOptionsSink {
