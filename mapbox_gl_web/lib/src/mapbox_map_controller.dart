0c464ead52010ceb78b791aa167b320700263d9e
Felix Horvat <felix.horvat@ocell.aero>
2022-01-13 19:04:36 +0100

    fixed issue with return type of remove source on web (#854)

--- mapbox_gl_web/lib/src/mapbox_map_controller.dart
+++ mapbox_gl_web/lib/src/mapbox_map_controller.dart
@@ -380,8 +380,8 @@ class MapboxMapController extends MapboxGlPlatform
   }
 
   @override
-  Future<void> removeSource(String sourceId) {
-    return _map.removeSource(sourceId);
+  Future<void> removeSource(String sourceId) async {
+    _map.removeSource(sourceId);
   }
 
   @override
