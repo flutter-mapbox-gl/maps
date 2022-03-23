4c0e2e221b3bc5b255f4bd486be8589d2996d4b2
AAverin <a.a.averin@gmail.com>
2022-03-23 22:47:48 +0100

    Do nothing and correcly return if layer and source to remove are not there

--- ios/Classes/MapboxMapController.swift
+++ ios/Classes/MapboxMapController.swift
@@ -528,7 +528,10 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
         case "style#removeSource":
             guard let arguments = methodCall.arguments as? [String: Any] else { return }
             guard let sourceId = arguments["sourceId"] as? String else { return }
-            guard let source = mapView.style?.source(withIdentifier: sourceId) else { return }
+            guard let source = mapView.style?.source(withIdentifier: sourceId) else {
+                result(nil)
+                return
+            }
             mapView.style?.removeSource(source)
             result(nil)
         case "style#addLayer":
@@ -709,7 +712,10 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
         case "style#removeLayer":
             guard let arguments = methodCall.arguments as? [String: Any] else { return }
             guard let layerId = arguments["layerId"] as? String else { return }
-            guard let layer = mapView.style?.layer(withIdentifier: layerId) else { return }
+            guard let layer = mapView.style?.layer(withIdentifier: layerId) else {
+                result(nil)
+                return
+            }
             interactiveFeatureLayerIds.remove(layerId)
             mapView.style?.removeLayer(layer)
             result(nil)
