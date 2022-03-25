a36db4a5c131534ce5500b69ef890727263068cc
Felix Horvat <felix.horvat@ocell.aero>
2022-03-25 15:42:44 +0100

    features with non string ids will not update properly (#970)

--- ios/Classes/MapboxMapController.swift
+++ ios/Classes/MapboxMapController.swift
@@ -1326,6 +1326,10 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
                         if let id = $0.identifier as? String,
                            let featureId = feature.identifier as? String
                         { return id == featureId }
+
+                        if let id = $0.identifier as? NSNumber,
+                           let featureId = feature.identifier as? NSNumber
+                        { return id == featureId }
                         return false
                     })
                 {
