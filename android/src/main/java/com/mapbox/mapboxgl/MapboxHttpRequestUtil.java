6e8f0b7c0885584955a77cfccdcacbf445978351
Tolga Guler <tolga.guler@netcad.com.tr>
2022-03-29 19:28:57 +0300

    java formatting

--- android/src/main/java/com/mapbox/mapboxgl/MapboxHttpRequestUtil.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxHttpRequestUtil.java
@@ -8,37 +8,38 @@ import io.flutter.plugin.common.MethodChannel;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-
 abstract class MapboxHttpRequestUtil {
 
+  public static void setHttpHeaders(Map<String, String> headers, MethodChannel.Result result) {
+    HttpRequestUtil.setOkHttpClient(getOkHttpClient(headers, result).build());
+    result.success(null);
+  }
 
-    public static void setHttpHeaders(Map<String, String> headers,
-                                      MethodChannel.Result result) {
-        HttpRequestUtil.setOkHttpClient(getOkHttpClient(headers, result).build());
-        result.success(null);
-    }
-
-    private static OkHttpClient.Builder getOkHttpClient(Map<String, String> headers, MethodChannel.Result result) {
-        try {
-            return new OkHttpClient.Builder().addNetworkInterceptor(chain -> {
+  private static OkHttpClient.Builder getOkHttpClient(
+      Map<String, String> headers, MethodChannel.Result result) {
+    try {
+      return new OkHttpClient.Builder()
+          .addNetworkInterceptor(
+              chain -> {
                 Request.Builder builder = chain.request().newBuilder();
                 for (Map.Entry<String, String> header : headers.entrySet()) {
-                    if (header.getKey() == null || header.getKey().trim().isEmpty()) {
-                        continue;
-                    }
-                    if (header.getValue() == null || header.getValue().trim().isEmpty()) {
-                        builder.removeHeader(header.getKey());
-                    } else {
-                        builder.header(header.getKey(), header.getValue());
-                    }
+                  if (header.getKey() == null || header.getKey().trim().isEmpty()) {
+                    continue;
+                  }
+                  if (header.getValue() == null || header.getValue().trim().isEmpty()) {
+                    builder.removeHeader(header.getKey());
+                  } else {
+                    builder.header(header.getKey(), header.getValue());
+                  }
                 }
                 return chain.proceed(builder.build());
-            });
-        } catch (Exception e) {
-            result.error("OK_HTTP_CLIENT_ERROR",
-                    "An unexcepted error happened during creating http " +
-                            "client" + e.getMessage(), null);
-            throw new RuntimeException(e);
-        }
+              });
+    } catch (Exception e) {
+      result.error(
+          "OK_HTTP_CLIENT_ERROR",
+          "An unexcepted error happened during creating http " + "client" + e.getMessage(),
+          null);
+      throw new RuntimeException(e);
     }
+  }
 }
