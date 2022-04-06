286ca0959caff408d617a44e5ab581daeb9ef915
Julien Grand-Chavin <hello@juliengrandchavin.dev>
2022-04-06 09:57:22 +0200

    🩹 Fix bug du click

--- example/lib/place_symbol.dart
+++ example/lib/place_symbol.dart
@@ -296,6 +296,7 @@ class PlaceSymbolBodyState extends State<PlaceSymbolBody> {
               initialCameraPosition: const CameraPosition(
                 target: LatLng(-33.852, 151.211),
                 zoom: 11.0,
+                tilt: 60,
               ),
             ),
           ),
