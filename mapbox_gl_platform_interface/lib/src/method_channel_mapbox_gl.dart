39d52cd1bf339df756bc98c84c2f835d2ca1c976
luminos <luminossrl@gmail.com>
2022-05-17 22:15:33 +0200

    Fixed Flutter 3.0.0 bug and resolved all related errors.

--- mapbox_gl_platform_interface/lib/src/method_channel_mapbox_gl.dart
+++ mapbox_gl_platform_interface/lib/src/method_channel_mapbox_gl.dart
@@ -141,7 +141,6 @@ class MethodChannelMapboxGl extends MapboxGlPlatform {
       OnPlatformViewCreatedCallback onPlatformViewCreated,
       Set<Factory<OneSequenceGestureRecognizer>>? gestureRecognizers) {
     if (defaultTargetPlatform == TargetPlatform.android) {
-      if (useHybridComposition) {
         return PlatformViewLink(
           viewType: 'plugins.flutter.io/mapbox_gl',
           surfaceFactory: (
@@ -156,8 +155,8 @@ class MethodChannelMapboxGl extends MapboxGlPlatform {
             );
           },
           onCreatePlatformView: (PlatformViewCreationParams params) {
-            final SurfaceAndroidViewController controller =
-                PlatformViewsService.initSurfaceAndroidView(
+            final ExpensiveAndroidViewController controller =
+                PlatformViewsService.initExpensiveAndroidView(
               id: params.id,
               viewType: 'plugins.flutter.io/mapbox_gl',
               layoutDirection: TextDirection.ltr,
@@ -176,15 +175,6 @@ class MethodChannelMapboxGl extends MapboxGlPlatform {
             return controller;
           },
         );
-      } else {
-        return AndroidView(
-          viewType: 'plugins.flutter.io/mapbox_gl',
-          onPlatformViewCreated: onPlatformViewCreated,
-          gestureRecognizers: gestureRecognizers,
-          creationParams: creationParams,
-          creationParamsCodec: const StandardMessageCodec(),
-        );
-      }
     } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       return UiKitView(
         viewType: 'plugins.flutter.io/mapbox_gl',
