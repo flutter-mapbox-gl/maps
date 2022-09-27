3c3be571df52eaf9887316c0c28729632f4ba175
Felix Horvat <felix.horvat@ocell.aero>
2022-09-27 14:40:16 +0200

    fix install script

--- install_formatting_tools.sh
+++ install_formatting_tools.sh
@@ -1,18 +1,17 @@
-if [ uname = "Linux" ]
+if [ $(uname) = "Linux" ]
 then
     echo "Install for Linux"
-    wget https://github.com/nicklockwood/SwiftFormat/releases/download/0.48.18/swiftformat.zip
-    unzip swiftformat.zip
-    rm swiftformat.zip
-    chomd +x swiftformat
-else
-    echo "Install for MAC"
     wget https://github.com/nicklockwood/SwiftFormat/releases/download/0.48.18/swiftformat_linux.zip
     unzip swiftformat_linux.zip
     rm swiftformat_linux.zip
-    chomd +x swiftformat_linux
     mv swiftformat_linux swiftformat
+else
+    echo "Install for MAC"
+    wget https://github.com/nicklockwood/SwiftFormat/releases/download/0.48.18/swiftformat.zip
+    unzip swiftformat.zip
+    rm swiftformat.zip
 fi
+chomd +x swiftformat
 
 rm google-java-format-1.13.0-all-deps.jar
 wget https://github.com/google/google-java-format/releases/download/v1.13.0/google-java-format-1.13.0-all-deps.jar
