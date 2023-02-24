35b7f88b456981594f04ce61444b14e868d840d1
Simon Irmančnik <simon@naviter.com>
2023-02-24 15:07:54 +0100

    Setting style as a json object directly for web

--- mapbox_gl_web/lib/mapbox_gl_web.dart
+++ mapbox_gl_web/lib/mapbox_gl_web.dart
@@ -1,6 +1,7 @@
 library mapbox_gl_web;
 
 import 'dart:async';
+import 'dart:convert';
 // FIXED HERE: https://github.com/dart-lang/linter/pull/1985
 // ignore_for_file: avoid_web_libraries_in_flutter
 import 'dart:html';
@@ -21,6 +22,7 @@ import 'package:mapbox_gl_platform_interface/mapbox_gl_platform_interface.dart';
 import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' hide Point, Source;
 import 'package:mapbox_gl_dart/mapbox_gl_dart.dart' as mapbox show Point;
 import 'package:image/image.dart' hide Point;
+import 'package:js/js_util.dart' as jsUtil;
 import 'package:mapbox_gl_web/src/layer_tools.dart';
 
 part 'src/convert.dart';
