dd20462f2db58a23f9a4bc0333f6a5398e57b479
Eduardo Folly <edufolly@gmail.com>
2023-03-30 14:58:16 -0300

    Fixing view_wrappers to Flutter 3.7. (#1273)

--- mapbox_gl_platform_interface/lib/src/view_wrappers.dart
+++ mapbox_gl_platform_interface/lib/src/view_wrappers.dart
@@ -34,6 +34,7 @@ class TextureAndroidViewControllerWrapper
 
   // @override
   PointTransformer get pointTransformer => _controller.pointTransformer;
+
   set pointTransformer(PointTransformer transformer) =>
       _controller.pointTransformer = transformer;
 
@@ -57,11 +58,14 @@ class TextureAndroidViewControllerWrapper
   /// size is the view's initial size in logical pixel. size can be omitted
   /// if the concrete implementation doesn't require an initial size to create
   /// the platform view.
-  Future<void> create({Size? size}) async {
+  Future<void> create({Size? size, Offset? position}) async {
     await _controller.create();
     awaitingCreation = false;
     if (size != null) {
       await _controller.setSize(size);
+      if (position != null) {
+        await _controller.setOffset(position);
+      }
     }
   }
 
@@ -123,6 +127,9 @@ class TextureAndroidViewControllerWrapper
 
   // @override
   int get viewId => _controller.viewId;
+
+  // @override
+  bool get requiresViewComposition => _controller.requiresViewComposition;
 }
 
 class AndroidViewWithWrappedController extends StatefulWidget {
