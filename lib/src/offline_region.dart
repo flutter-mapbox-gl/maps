b676ed7315143cf003a42bf7b586d41acfcddba9
Utsav Porwal <utsavporwal01@gmail.com>
2023-08-16 11:41:38 +0530

    Update offline_region.dart

--- lib/src/offline_region.dart
+++ lib/src/offline_region.dart
@@ -30,7 +30,7 @@ class OfflineRegionDefinition {
     data['minZoom'] = minZoom;
     data['maxZoom'] = maxZoom;
     data['includeIdeographs'] = includeIdeographs;
-    data['vectorSourceProperties'] = vectorSourceProperties;
+    data['vectorSourceProperties'] = vectorSourceProperties.toJson();
     return data;
   }
 
