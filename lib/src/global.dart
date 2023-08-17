ba713e3273ee82958102e84083aebbd169b38c6c
utsav.porwal <utsavporwal01@gmail.com>
2023-08-17 11:55:32 +0530

    kotlin channel added

--- lib/src/global.dart
+++ lib/src/global.dart
@@ -4,165 +4,181 @@
 
 part of mapbox_gl;
 
-final MethodChannel _globalChannel =
-    MethodChannel('plugins.flutter.io/mapbox_gl');
+// final MethodChannel _globalChannel =
+//     MethodChannel('plugins.flutter.io/mapbox_gl');
+static const platform=MethodChannel("offline_manager_plugin");
+
+void offlineManager() async{
+  String value=" ";
+  try{
+    value=await platform.invokeMethod("download_tileset");
+  }catch(e){
+    if (kDebugMode) {
+      print(e);
+    }
+  }
+  if (kDebugMode) {
+    print(value);
+  }
+}
+
 
 /// Copy tiles db file passed in to the tiles cache directory (sideloaded) to
 /// make tiles available offline.
-Future<void> installOfflineMapTiles(String tilesDb) async {
-  await _globalChannel.invokeMethod(
-    'installOfflineMapTiles',
-    <String, dynamic>{
-      'tilesdb': tilesDb,
-    },
-  );
-}
-
-enum DragEventType { start, drag, end }
-
-Future<dynamic> setOffline(
-  bool offline, {
-  String? accessToken,
-}) =>
-    _globalChannel.invokeMethod(
-      'setOffline',
-      <String, dynamic>{
-        'offline': offline,
-        'accessToken': accessToken,
-      },
-    );
-
-Future<void> setHttpHeaders(Map<String, String> headers) {
-  return _globalChannel.invokeMethod(
-    'setHttpHeaders',
-    <String, dynamic>{
-      'headers': headers,
-    },
-  );
-}
-
-Future<List<OfflineRegion>> mergeOfflineRegions(
-  String path, {
-  String? accessToken,
-}) async {
-  String regionsJson = await _globalChannel.invokeMethod(
-    'mergeOfflineRegions',
-    <String, dynamic>{
-      'path': path,
-      'accessToken': accessToken,
-    },
-  );
-  Iterable regions = json.decode(regionsJson);
-  return regions.map((region) => OfflineRegion.fromMap(region)).toList();
-}
-
-Future<List<OfflineRegion>> getListOfRegions({String? accessToken}) async {
-  String regionsJson = await _globalChannel.invokeMethod(
-    'getListOfRegions',
-    <String, dynamic>{
-      'accessToken': accessToken,
-    },
-  );
-  Iterable regions = json.decode(regionsJson);
-  return regions.map((region) => OfflineRegion.fromMap(region)).toList();
-}
-
-Future<OfflineRegion> updateOfflineRegionMetadata(
-  int id,
-  Map<String, dynamic> metadata, {
-  String? accessToken,
-}) async {
-  final regionJson = await _globalChannel.invokeMethod(
-    'updateOfflineRegionMetadata',
-    <String, dynamic>{
-      'id': id,
-      'accessToken': accessToken,
-      'metadata': metadata,
-    },
-  );
-
-  return OfflineRegion.fromMap(json.decode(regionJson));
-}
-
-Future<dynamic> setOfflineTileCountLimit(int limit, {String? accessToken}) =>
-    _globalChannel.invokeMethod(
-      'setOfflineTileCountLimit',
-      <String, dynamic>{
-        'limit': limit,
-        'accessToken': accessToken,
-      },
-    );
-
-Future<dynamic> deleteOfflineRegion(int id, {String? accessToken}) =>
-    _globalChannel.invokeMethod(
-      'deleteOfflineRegion',
-      <String, dynamic>{
-        'id': id,
-        'accessToken': accessToken,
-      },
-    );
-
-Future<OfflineRegion> downloadOfflineRegion(
-  OfflineRegionDefinition definition, {
-  Map<String, dynamic> metadata = const {},
-  String? accessToken,
-  Function(DownloadRegionStatus event)? onEvent,
-}) async {
-  String channelName =
-      'downloadOfflineRegion_${DateTime.now().microsecondsSinceEpoch}';
-
-  final result = await _globalChannel
-      .invokeMethod('downloadOfflineRegion', <String, dynamic>{
-    'accessToken': accessToken,
-    'channelName': channelName,
-    'definition': definition.toMap(),
-    'metadata': metadata,
-  });
-
-  if (onEvent != null) {
-    EventChannel(channelName).receiveBroadcastStream().handleError((error) {
-      if (error is PlatformException) {
-        onEvent(Error(error));
-        return Error(error);
-      }
-      var unknownError = Error(
-        PlatformException(
-          code: 'UnknowException',
-          message:
-              'This error is unhandled by plugin. Please contact us if needed.',
-          details: error,
-        ),
-      );
-      onEvent(unknownError);
-      return unknownError;
-    }).listen((data) {
-      final Map<String, dynamic> jsonData = json.decode(data);
-      DownloadRegionStatus? status;
-      switch (jsonData['status']) {
-        case 'start':
-          status = InProgress(0.0);
-          break;
-        case 'progress':
-          final dynamic value = jsonData['progress'];
-          double progress = 0.0;
-
-          if (value is int) {
-            progress = value.toDouble();
-          }
-
-          if (value is double) {
-            progress = value;
-          }
-
-          status = InProgress(progress);
-          break;
-        case 'success':
-          status = Success();
-          break;
-      }
-      onEvent(status ?? (throw 'Invalid event status ${jsonData['status']}'));
-    });
-  }
-
-  return OfflineRegion.fromMap(json.decode(result));
-}
+// Future<void> installOfflineMapTiles(String tilesDb) async {
+//   await _globalChannel.invokeMethod(
+//     'installOfflineMapTiles',
+//     <String, dynamic>{
+//       'tilesdb': tilesDb,
+//     },
+//   );
+// }
+//
+// enum DragEventType { start, drag, end }
+//
+// Future<dynamic> setOffline(
+//   bool offline, {
+//   String? accessToken,
+// }) =>
+//     _globalChannel.invokeMethod(
+//       'setOffline',
+//       <String, dynamic>{
+//         'offline': offline,
+//         'accessToken': accessToken,
+//       },
+//     );
+//
+// Future<void> setHttpHeaders(Map<String, String> headers) {
+//   return _globalChannel.invokeMethod(
+//     'setHttpHeaders',
+//     <String, dynamic>{
+//       'headers': headers,
+//     },
+//   );
+// }
+//
+// Future<List<OfflineRegion>> mergeOfflineRegions(
+//   String path, {
+//   String? accessToken,
+// }) async {
+//   String regionsJson = await _globalChannel.invokeMethod(
+//     'mergeOfflineRegions',
+//     <String, dynamic>{
+//       'path': path,
+//       'accessToken': accessToken,
+//     },
+//   );
+//   Iterable regions = json.decode(regionsJson);
+//   return regions.map((region) => OfflineRegion.fromMap(region)).toList();
+// }
+//
+// Future<List<OfflineRegion>> getListOfRegions({String? accessToken}) async {
+//   String regionsJson = await _globalChannel.invokeMethod(
+//     'getListOfRegions',
+//     <String, dynamic>{
+//       'accessToken': accessToken,
+//     },
+//   );
+//   Iterable regions = json.decode(regionsJson);
+//   return regions.map((region) => OfflineRegion.fromMap(region)).toList();
+// }
+//
+// Future<OfflineRegion> updateOfflineRegionMetadata(
+//   int id,
+//   Map<String, dynamic> metadata, {
+//   String? accessToken,
+// }) async {
+//   final regionJson = await _globalChannel.invokeMethod(
+//     'updateOfflineRegionMetadata',
+//     <String, dynamic>{
+//       'id': id,
+//       'accessToken': accessToken,
+//       'metadata': metadata,
+//     },
+//   );
+//
+//   return OfflineRegion.fromMap(json.decode(regionJson));
+// }
+//
+// Future<dynamic> setOfflineTileCountLimit(int limit, {String? accessToken}) =>
+//     _globalChannel.invokeMethod(
+//       'setOfflineTileCountLimit',
+//       <String, dynamic>{
+//         'limit': limit,
+//         'accessToken': accessToken,
+//       },
+//     );
+//
+// Future<dynamic> deleteOfflineRegion(int id, {String? accessToken}) =>
+//     _globalChannel.invokeMethod(
+//       'deleteOfflineRegion',
+//       <String, dynamic>{
+//         'id': id,
+//         'accessToken': accessToken,
+//       },
+//     );
+//
+// Future<OfflineRegion> downloadOfflineRegion(
+//   OfflineRegionDefinition definition, {
+//   Map<String, dynamic> metadata = const {},
+//   String? accessToken,
+//   Function(DownloadRegionStatus event)? onEvent,
+// }) async {
+//   String channelName =
+//       'downloadOfflineRegion_${DateTime.now().microsecondsSinceEpoch}';
+//
+//   final result = await _globalChannel
+//       .invokeMethod('downloadOfflineRegion', <String, dynamic>{
+//     'accessToken': accessToken,
+//     'channelName': channelName,
+//     'definition': definition.toMap(),
+//     'metadata': metadata,
+//   });
+//
+//   if (onEvent != null) {
+//     EventChannel(channelName).receiveBroadcastStream().handleError((error) {
+//       if (error is PlatformException) {
+//         onEvent(Error(error));
+//         return Error(error);
+//       }
+//       var unknownError = Error(
+//         PlatformException(
+//           code: 'UnknowException',
+//           message:
+//               'This error is unhandled by plugin. Please contact us if needed.',
+//           details: error,
+//         ),
+//       );
+//       onEvent(unknownError);
+//       return unknownError;
+//     }).listen((data) {
+//       final Map<String, dynamic> jsonData = json.decode(data);
+//       DownloadRegionStatus? status;
+//       switch (jsonData['status']) {
+//         case 'start':
+//           status = InProgress(0.0);
+//           break;
+//         case 'progress':
+//           final dynamic value = jsonData['progress'];
+//           double progress = 0.0;
+//
+//           if (value is int) {
+//             progress = value.toDouble();
+//           }
+//
+//           if (value is double) {
+//             progress = value;
+//           }
+//
+//           status = InProgress(progress);
+//           break;
+//         case 'success':
+//           status = Success();
+//           break;
+//       }
+//       onEvent(status ?? (throw 'Invalid event status ${jsonData['status']}'));
+//     });
+//   }
+//
+//   return OfflineRegion.fromMap(json.decode(result));
+// }
