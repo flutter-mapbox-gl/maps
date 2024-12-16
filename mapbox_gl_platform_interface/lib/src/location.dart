ddcc256a6b178704b928cc05d27acbf205e3cc33
Kelven G <kelvengalvao@gmail.com>
2024-12-16 14:51:17 -0300

    dart format

--- mapbox_gl_platform_interface/lib/src/location.dart
+++ mapbox_gl_platform_interface/lib/src/location.dart
@@ -14,7 +14,8 @@ class LatLng {
   /// The longitude is normalized to the half-open interval from -180.0
   /// (inclusive) to +180.0 (exclusive)
   const LatLng(double latitude, double longitude)
-      : latitude = (latitude < -90.0 ? -90.0 : (90.0 < latitude ? 90.0 : latitude)),
+      : latitude =
+            (latitude < -90.0 ? -90.0 : (90.0 < latitude ? 90.0 : latitude)),
         longitude = (longitude + 180.0) % 360.0 - 180.0;
 
   /// The latitude in degrees between -90.0 and 90.0, both inclusive.
@@ -99,7 +100,9 @@ class LatLngBounds {
 
   @override
   bool operator ==(Object o) {
-    return o is LatLngBounds && o.southwest == southwest && o.northeast == northeast;
+    return o is LatLngBounds &&
+        o.southwest == southwest &&
+        o.northeast == northeast;
   }
 
   @override
